6f22a9d2a1a76c5055288f01c317bd620be8bf02
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2025-03-08 21:07:55 +0300

    Исправлены замечания после ревью

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,6 +4,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
